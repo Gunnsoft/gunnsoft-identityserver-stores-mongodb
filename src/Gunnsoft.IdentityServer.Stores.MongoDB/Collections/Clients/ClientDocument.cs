53cd3a7fe06d08e9c7f86ef871a4ade47a088b71
Andrew Gunn <hello@andrewgunn.co.uk>
2018-09-28 19:44:12 +0100

    Instantiate list properties

--- src/Gunnsoft.IdentityServer.Stores.MongoDB/Collections/Clients/ClientDocument.cs
+++ src/Gunnsoft.IdentityServer.Stores.MongoDB/Collections/Clients/ClientDocument.cs
@@ -11,9 +11,9 @@ namespace Gunnsoft.IdentityServer.Stores.MongoDB.Collections.Clients
         public int AccessTokenLifetime { get; set; } = 3600;
         public AccessTokenType AccessTokenType { get; set; } = AccessTokenType.Jwt;
         public bool AllowAccessTokensViaBrowser { get; set; }
-        public List<string> AllowedCorsOrigins { get; set; }
-        public List<string> AllowedGrantTypes { get; set; }
-        public List<string> AllowedScopes { get; set; }
+        public List<string> AllowedCorsOrigins { get; set; } = new List<string>();
+        public List<string> AllowedGrantTypes { get; set; } = new List<string>();
+        public List<string> AllowedScopes { get; set; } = new List<string>();
         public bool AllowOfflineAccess { get; set; }
         public bool AllowPlainTextPkce { get; set; }
         public bool AllowRememberConsent { get; set; } = true;
@@ -22,11 +22,11 @@ namespace Gunnsoft.IdentityServer.Stores.MongoDB.Collections.Clients
         public int AuthorizationCodeLifetime { get; set; } = 300;
         public bool BackChannelLogoutSessionRequired { get; set; } = true;
         public string BackChannelLogoutUri { get; set; }
-        public List<ClientClaim> Claims { get; set; }
+        public List<ClientClaim> Claims { get; set; } = new List<ClientClaim>();
         public string ClientClaimsPrefix { get; set; } = "client_";
         public string ClientId { get; set; }
         public string ClientName { get; set; }
-        public List<ClientSecret> ClientSecrets { get; set; }
+        public List<ClientSecret> ClientSecrets { get; set; } = new List<ClientSecret>();
         public string ClientUri { get; set; }
         public int? ConsentLifetime { get; set; } = null;
         public bool Enabled { get; set; } = true;
@@ -34,15 +34,15 @@ namespace Gunnsoft.IdentityServer.Stores.MongoDB.Collections.Clients
         public bool FrontChannelLogoutSessionRequired { get; set; } = true;
         public string FrontChannelLogoutUri { get; set; }
         public ObjectId Id { get; set; }
-        public List<string> IdentityProviderRestrictions { get; set; }
+        public List<string> IdentityProviderRestrictions { get; set; } = new List<string>();
         public int IdentityTokenLifetime { get; set; } = 300;
         public bool IncludeJwtId { get; set; }
         public string LogoUri { get; set; }
         public string PairWiseSubjectSalt { get; set; }
-        public List<string> PostLogoutRedirectUris { get; set; }
-        public List<ClientProperty> Properties { get; set; }
+        public List<string> PostLogoutRedirectUris { get; set; } = new List<string>();
+        public List<ClientProperty> Properties { get; set; } = new List<ClientProperty>();
         public string ProtocolType { get; set; } = IdentityServerConstants.ProtocolTypes.OpenIdConnect;
-        public List<string> RedirectUris { get; set; }
+        public List<string> RedirectUris { get; set; } = new List<string>();
         public TokenExpiration RefreshTokenExpiration { get; set; } = TokenExpiration.Absolute;
         public TokenUsage RefreshTokenUsage { get; set; } = TokenUsage.OneTimeOnly;
         public bool RequireClientSecret { get; set; } = true;
